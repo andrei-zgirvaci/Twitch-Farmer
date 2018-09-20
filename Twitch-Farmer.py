01693fcf7ee121bee1201f217ace3ded05ea5945
Andrei Zgirvaci <zgirvaciandrei@gmail.com>
2018-09-21 00:52:05 +0200

    update xPaths and include chromedriver

--- Twitch-Farmer.py
+++ Twitch-Farmer.py
@@ -1,4 +1,7 @@
 import time
+import os
+from pathlib import Path
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,12 +13,13 @@ def SetUp(Proxy, username, password):
     global driver
     print("using proxy: " + str(Proxy) + '\n')
 
+    chrome_driver_path = Path("{}/drivers/chromedriver".format(os.getcwd()))
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--proxy-server=http://%s' % Proxy)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    #driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver_path)
 
-    #driver.set_page_load_timeout(15)
+    # driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
     driver.set_page_load_timeout(20)
 
@@ -50,25 +54,25 @@ def Login(twitchUsername, twitchPassword):
         print ("")     
 
     global channelXpath
-    global change_Account
     global delay
-    global searchFieldID
+    global myAccountXpath
     global incorrectCapthaXpath
     global incorrectUserXpath
 
-    usernameFieldID = "username"
-    passwordFieldName = "password"
-    loginButtonXpath = '//*[@id="loginForm"]/div[3]/button'
-    incorrectCapthaXpath = "//*[@id='loginForm']/div[contains(text(), 'Please complete the CAPTCHA correctly.')]"
-    incorrectUserXpath = "//*[@id='loginForm']/div[contains(text(), 'Incorrect username or password.')]"
-    searchFieldID = "query"
-    channelXpath= "//*[@id='channel']/div/div[2]/p"  
+    usernameFieldXpath = "//div[contains(@data-a-target,'login-username-input')]/input"
+    passwordFieldXpath = "//div[contains(@data-a-target,'login-password-input')]/input"
+    loginButtonXpath = "//button[contains(@data-a-target,'passport-login-button')]"
+    incorrectUserXpath = "//div[contains(@class, 'server-message-alert')]"
+    myAccountXpath = "//div[contains(@data-a-target, 'user-display-name')]"
+    incorrectCapthaXpath = "//div[contains(@id, 'recaptcha-element-container')]"
+    channelXpath= "//div[contains(@data-target, 'channel-header-right')]//button[contains(@data-a-target, 'follow-button')]"
+    
     delay = 15
 
     # checks if login page is loaded
     try:
-        usernameFieldElement = WebDriverWait(driver, delay + 5).until(EC.presence_of_element_located((By.ID, usernameFieldID)))
-        passwordFieldElement = driver.find_element_by_name(passwordFieldName)
+        usernameFieldElement = WebDriverWait(driver, delay + 5).until(EC.presence_of_element_located((By.XPATH, usernameFieldXpath)))
+        passwordFieldElement = driver.find_element_by_xpath(passwordFieldXpath)
         loginButtonElement = driver.find_element_by_xpath(loginButtonXpath)
         usernameFieldElement.clear()
         usernameFieldElement.send_keys(twitchUsername)
@@ -87,8 +91,8 @@ def Logged(twitchUsername, twitchPassword):
 
     #checks if logged
     try:
-        WebDriverWait(driver, delay - 5).until(EC.presence_of_element_located((By.ID, searchFieldID)))
-        Follow()
+        WebDriverWait(driver, delay - 5).until(EC.presence_of_element_located((By.XPATH, myAccountXpath)))
+        Follow(twitchUsername, twitchPassword)
     except TimeoutException:
         try:
             driver.find_element_by_xpath(incorrectCapthaXpath)
@@ -108,12 +112,12 @@ def Logged(twitchUsername, twitchPassword):
                     print("------------------------------------------- " + '\n')
                     change_proxy(twitchUsername, twitchPassword)
 
-def Follow():
+def Follow(twitchUsername, twitchPassword):
     driver.get("https://www.twitch.tv/" + str(channel))
 
     #checks if channel is found
     try:
-        followButtonElement = WebDriverWait(driver, delay - 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Follow")))
+        followButtonElement = WebDriverWait(driver, delay - 5).until(EC.presence_of_element_located((By.XPATH, channelXpath)))
         followButtonElement.click()
         time.sleep(2)
         print ("Folowing, moving on the next user!!!" + '\n')
@@ -122,8 +126,8 @@ def Follow():
     except TimeoutException:
         #checks if channel is already following
         try:
-            driver.find_element_by_class_name("svg-heart")
-            print ("This user is already following thin channel, moving on the next user!!!" + '\n')
+            driver.find_element_by_xpath("//button[contains(@data-a-target, 'unfollow-button')]")
+            print ("This user is already following this channel, moving on the next user!!!" + '\n')
             print("------------------------------------------- " + '\n')
             driver.quit()
         except:
@@ -138,12 +142,12 @@ def Follow():
                 change_proxy(twitchUsername, twitchPassword)             
 #__main__#
 #global proxy_count
-#global change_Account
+global change_Account
 change_Account = False
 proxy_count = 0
 
 f_proxies = open("proxies.txt", "r")
-proxies = f_proxies.read().splitlines()
+proxies = f_proxies.read().splitlines()[1:]
 if (proxies[-1] != "end"):
     with open("proxies.txt", 'a') as f_proxies:
         f_proxies.write('\n' + "end")
@@ -152,11 +156,11 @@ f_proxies.close()
 channel = input('what channel should i follow?: ')
 
 with open('users.txt') as f_users:
-  credentials = [x.strip().split(':') for x in f_users.readlines()]
+  credentials = [x.strip().split(':') for x in f_users.readlines()[1:]]
 
 for username, password in credentials:
         if (proxies[proxy_count] != "end"):
-            if (change_Account == True): 
+            if (change_Account == True):
                 change_Account = False  
                 SetUp(oldProxy, oldUsername, oldPassword)
             else:
