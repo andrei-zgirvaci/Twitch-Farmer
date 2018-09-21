9296f73b9d34727bfad0de6e85fd42d3688e244a
Andrei Zgirvaci <zgirvaciandrei@gmail.com>
2018-09-21 23:14:25 +0200

    working on version 2.0

--- twitch_farmer.py
+++ twitch_farmer.py
@@ -15,22 +15,102 @@ from selenium.common.exceptions import TimeoutException
 driver = None
 current_proxy = None
 proxy_timeout = False
+delay = 10
+page_elements = None
 
 
-def follow_channel(follow_channel_name):
-    pass
+def follow_channel(username, follow_channel_name):
+    global delay
+
+    print("[{}] Trying to follow: [{}]".format(username, follow_channel_name))
+
+    try:
+        driver.get("https://www.twitch.tv/{}".format(follow_channel_name))
+
+        follow_button = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.XPATH, get_page_element("follow_button"))))
+
+        follow_button.click()
+
+        print("[{}] Successfully followed: [{}]".format(
+            username, follow_channel_name))
+
+        return True
+    except:
+        # checks if channel is already following
+        try:
+            driver.find_element_by_xpath(get_page_element("unfollow_button"))
+
+            print("[{}] Is already following: [{}], moving on the next user!!!".format(
+                username, follow_channel_name))
+        except:
+            try:
+                driver.find_element_by_xpath(
+                    get_page_element("following_account"))
+                print("No channel: [{}] found, exiting!!!".format(
+                    follow_channel_name))
+                exit()
+            except:
+                pass
+
+            return False
 
 
 def sign_in(username, passowrd):
-    print("Singing In - " + username)
+    global proxy_timeout
+    global delay
 
-    pass
+    print("[{}] Singing in...".format(username))
+
+    try:
+        driver.get("https://www.twitch.tv/login")
+        # driver.get("http://whatismyipaddress.com")
+
+        username_input = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.XPATH, get_page_element("username_input"))))
+
+        password_input = driver.find_element_by_xpath(
+            get_page_element("password_input"))
+        login_button = driver.find_element_by_xpath(
+            get_page_element("login_button"))
+
+        username_input.clear()
+        username_input.send_keys(username)
+        password_input.clear()
+        password_input.send_keys(passowrd)
+
+        login_button.click()
+
+        WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+            (By.XPATH, get_page_element("my_account"))))
+
+        print("[{}] Successfully signed in.\n".format(username))
+        return True
+    except TimeoutException:
+        try:
+            driver.find_element_by_xpath(get_page_element("auth_failed"))
+            print("[{}] Incorrect credentials, changing account!!!".format(username))
+            return False
+        except:
+            try:
+                driver.find_element_by_xpath(get_page_element("recaptcha"))
+                print("[{}] Recaptcha, changing proxy!!!".format(username))
+            except:
+                pass
+
+            proxy_timeout = True
+            return False
 
 
 def main():
     global proxy_timeout
+    global page_elements
 
     follow_channel_name = input("What channel should I follow? : ")
+
+    # load page_elements json file
+    with open(get_full_path("/page_elements.json")) as page_elements_file:
+        page_elements = json.load(page_elements_file)
 
     accounts_csv_path = get_full_path("/data/accounts.csv")
     accounts_csv = pandas.read_csv(accounts_csv_path, sep=',', dtype="str")
@@ -46,32 +126,66 @@ def main():
         # change proxy
         run_driver(True)
 
-        while sign_in(username, password) is False and proxy_timeout:
-            print("\nTime expired, changing proxy...")
+        sign_in_status = sign_in(username, password)
+
+        while sign_in_status is False and proxy_timeout:
+            print("--------------------------------")
+            print("Time expired, changing proxy...")
             print("Using previous account: [{}]\n".format(username))
 
+            # change proxy
             run_driver(True)
 
-        print("[{}] Successfully signed in.".format(username))
+            sign_in_status = sign_in(username, password)
 
-        print("[{}] Trying to follow: [{}]".format(
-            username, follow_channel_name))
+        if sign_in_status:
+            if follow_channel(username, follow_channel_name):
+                accounts_csv.at[i, 'following_channel'] = follow_channel_name
+                accounts_csv.at[i, 'used_proxy'] = current_proxy
+                accounts_csv.to_csv(accounts_csv_path, sep=',',
+                                    index=False, index_label=False)
+            else:
+                accounts_csv.at[i, 'following_channel'] = "ERROR"
+                accounts_csv.to_csv(accounts_csv_path, sep=',',
+                                    index=False, index_label=False)
 
-        if follow_channel(follow_channel_name):
-            accounts_csv.at[i, 'following_channel'] = follow_channel
-            accounts_csv.at[i, 'used_proxy'] = current_proxy
-            accounts_csv.to_csv(accounts_csv_path, sep=',',
-                                index=False, index_label=False)
+                print("[{}] An error ocured while trying to follow: [{}]!!!".format(
+                    username, follow_channel_name))
+    print("--------------------------------")
+    print("No more accounts!!!")
+    print("--------------------------------")
 
-            print("[{}] Successfully followed: [{}]".format(
-                username, follow_channel_name))
-        else:
-            accounts_csv.at[i, 'following_channel'] = "ERROR"
-            accounts_csv.to_csv(accounts_csv_path, sep=',',
-                                index=False, index_label=False)
 
-            print("[{}] An error ocured while trying to follow: [{}]!!!".format(
-                username, follow_channel_name))
+def get_page_element(input):
+    global page_elements
+
+    return page_elements[input]["value"]
+
+
+def get_full_path(file_path):
+    return Path("{}{}".format(os.getcwd(), file_path))
+
+
+def find_available_proxy():
+    proxies_csv_path = get_full_path("/data/proxies.csv")
+    proxies_csv = pandas.read_csv(proxies_csv_path, sep=',', dtype="str")
+
+    # define proxies.csv columns
+    proxy_rows = proxies_csv.proxy
+    status_rows = proxies_csv.status
+
+    # go through every proxy in .csv and check if it is available to use
+    for i, (proxy, status) in enumerate(zip(proxy_rows, status_rows)):
+        if i == len(proxy_rows) - 1 and status == "USED":
+            print("--------------------------------")
+            print("No more proxies!!!")            
+            print("--------------------------------")
+            exit()
+        elif pandas.isnull(status):
+            proxies_csv.at[i, 'status'] = "USED"
+            proxies_csv.to_csv(proxies_csv_path, sep=',',
+                               index=False, index_label=False)
+            return proxy
 
 
 def run_driver(run_with_proxy):
@@ -91,11 +205,7 @@ def run_driver(run_with_proxy):
         if current_proxy:
             chrome_options.add_argument(
                 '--proxy-server=socks5://' + current_proxy)
-
-            print("New Proxy: " + current_proxy)
-        else:
-            print("No more proxies!!!")
-            exit()
+            print("Using proxy: [{}]".format(current_proxy))
 
     chrome_driver_path = get_full_path("/drivers/chromedriver")
 
@@ -104,31 +214,6 @@ def run_driver(run_with_proxy):
     driver.set_window_size(880, 720)
     driver.set_window_position(0, 0)
     driver.set_page_load_timeout(20)
-
-
-def find_available_proxy():
-    proxies_csv_path = get_full_path("/data/proxies.csv")
-    proxies_csv = pandas.read_csv(proxies_csv_path, sep=',', dtype="str")
-
-    # define proxies.csv columns
-    proxy_rows = proxies_csv.proxy
-    status_rows = proxies_csv.status
-
-    # go through every proxy in .csv and check if it is available to use
-    for i, (proxy, status) in enumerate(zip(proxy_rows, status_rows)):
-        if i == len(proxy_rows) - 1 and status == "USED":
-            return False
-        elif pandas.isnull(status):
-            proxies_csv.at[i, 'status'] = "USED"
-            proxies_csv.to_csv(proxies_csv_path, sep=',',
-                               index=False, index_label=False)
-            return proxy
-
-
-def exit():
-    stop_driver()
-    input("Pres 'Enter' to Exit")
-    sys.exit()
 
 
 def stop_driver():
@@ -141,12 +226,12 @@ def stop_driver():
         driver = None
 
 
-def get_full_path(file_path):
-    return Path("{}{}".format(os.getcwd(), file_path))
+def exit():
+    stop_driver()
+    input("Pres 'Enter' to Exit")
+    sys.exit()
 
 
 if __name__ == '__main__':
     main()
-    print("--------------------------------")
-    print("No more accounts!!!")
     exit()
